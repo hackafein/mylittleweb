2d342fb04c57d1449d95c80e0b350d820cb051a6
Hüseyin Furkan Ceran <42136880+hackafein@users.noreply.github.com>
2025-02-12 11:11:41 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 
 # OpenAI Web Arayüzü URL'si
-CHATGPT_URL = "http://chat.deepseek.com/"
+CHATGPT_URL = "https://chat.openai.com/"
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>", methods=["GET", "POST", "PUT", "DELETE"])
@@ -13,6 +13,7 @@ def proxy(path):
    url = f"{CHATGPT_URL}/{path}"
 
    headers = {key: value for key, value in request.headers if key != "Host"}
+   headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64;) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.75 Safari/537.36"
    data = request.get_data() if request.method != "GET" else None
 
    response = requests.request(
@@ -20,8 +21,8 @@ def proxy(path):
        url=url,
        headers=headers,
        data=data,
-       cookies=request.cookies,
-       allow_redirects=True
+       cookies=request.cookies if "cf_clearance" in request.cookies else None,
+       allow_redirects=False
    )
 
    return response.content, response.status_code, response.headers.items()
